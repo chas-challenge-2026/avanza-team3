41fb9a72be8fbf6d54f9fdf8e885dbe421467ea7
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-08-24 15:44:34 +0200

    test placement on card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import Card from "./components/Card";
 import BaseLayout from "./layouts/BaseLayout";
 
-
 function App() {
-  return <>
-  <BaseLayout/>
-  </>;
+  return (
+    <>
+      <BaseLayout />
+      <Card>
+        <h2>Card Title</h2>
+        <p>Content</p>
+      </Card>
+    </>
+  );
 }
 
 export default App;
