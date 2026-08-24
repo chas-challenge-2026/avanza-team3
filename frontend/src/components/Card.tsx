0f50d2fec6ba0ed76b0bd1e01ee43e907bd93585
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-08-24 14:11:32 +0200

    card component

---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+
+type CardProps = {
+  children: React.ReactNode;
+};
+
+const Card = ({ children }: CardProps) => {
+  return <div>{children}</div>;
+};
+export default Card;
