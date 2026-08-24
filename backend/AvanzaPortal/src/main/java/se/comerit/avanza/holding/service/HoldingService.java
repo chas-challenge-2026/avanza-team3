058f814359675ac7e7d21f07a21e085ac854d123
kostas900402 <nascoskostas@gmail.com>
2026-08-24 11:14:59 +0200

    holdingservice()

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/holding/service/HoldingService.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/holding/service/HoldingService.java
@@ -1,14 +1,18 @@
 package se.comerit.avanza.holding.service;
 
+import org.springframework.stereotype.Service;
+import se.comerit.avanza.holding.repository.HoldingRepository;
+
+@Service
 public class HoldingService {
 
-    /*
-    i service-klasser körs all logik med metoder som getAllHoldings(),
-    getHoldingById(), deleteHoldingById(), etc.
-     */
+    private final HoldingRepository holdingRepository;
 
-    //Utöver detta exempelpaketet så kommer vi behöva paket för
-    // gemensamma exceptions, util-paket, security-paket, etc.
-    //Exakt struktur behöver inte vara exakt så här men detta är grundtanken
+    public HoldingService(HoldingRepository holdingRepository) {
+        this.holdingRepository = holdingRepository;
+    }
+
+
+
 
 }
