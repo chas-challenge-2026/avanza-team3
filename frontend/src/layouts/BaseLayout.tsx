6025642d495f462b89db86a4f0177f6976b17f0c
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-08-25 14:38:08 +0200

    created header component with module. updated button and card with css. added in to baselayout.

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,19 +1,18 @@
-import AppButton from "../components/AppButton";
 import AppCard from "../components/AppCard";
+import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import styles from "./BaseLayout.module.css";
 
 const BaseLayout = () => {
   return (
-    <div className="app-layout">
-      {/* <Navbar /> */}
-      <div className="app-body">
-        {/* <Header/> */}
-        <Sidebar />
-        <main className="main-content">
-          <AppCard variant="elevation">
+    <div className={styles.appLayout}>
+      <Sidebar />
+      <div className={styles.appBody}>
+        <Header userName="Sascha B" />
+        <main className={styles.mainContent}>
+          <AppCard>
             <h2>Card Title</h2>
-            <p>Card Description</p>
-            <AppButton variant="contained">Spara</AppButton>
+            <p>Card Content</p>
           </AppCard>
         </main>
       </div>
