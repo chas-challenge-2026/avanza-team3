e8300e6a3669f44266269c0a70de1788b58ce209
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-08-27 15:54:45 +0200

    added disabled prop on button component

--- frontend/src/components/AppButton.tsx
+++ frontend/src/components/AppButton.tsx
@@ -7,11 +7,18 @@ type AppButtonProps = {
   variant?: "outlined" | "contained";
   sx?: SxProps<Theme>;
   type?: "button" | "submit";
+  disabled?: boolean;
 };
 
-const AppButton = ({ children, variant, sx, type }: AppButtonProps) => {
+const AppButton = ({
+  children,
+  variant,
+  sx,
+  type,
+  disabled
+}: AppButtonProps) => {
   return (
-    <Button sx={sx} variant={variant} type={type}>
+    <Button sx={sx} variant={variant} type={type} disabled={disabled}>
       {children}
     </Button>
   );
