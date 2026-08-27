0b78a9654c8e7920cec6eec35f0c6626fa7a6b3d
kostas900402 <nascoskostas@gmail.com>
2026-08-27 14:05:01 +0200

    getAccountByIdForUser()

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/repository/AccountRepository.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/repository/AccountRepository.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Repository;
 import se.comerit.avanza.account.model.Account;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 
     List<Account> findByUserIdOrderByAccountTypeAscAccountNameAsc(Integer userId);
+
+    Optional<Account> findByIdAndUserId(Integer id, Integer userId);
 
 }
--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/service/AccountService.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/service/AccountService.java
@@ -1,5 +1,7 @@
 package se.comerit.avanza.account.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import se.comerit.avanza.account.model.Account;
 import se.comerit.avanza.account.repository.AccountRepository;
 
@@ -16,4 +18,11 @@ public class AccountService {
     public List<Account> getAccountsByUserId(Integer userId) {
         return accountRepository.findByUserIdOrderByAccountTypeAscAccountNameAsc(userId);
     }
+
+    public Account getAccountByIdForUser(Integer accountId, Integer userId) {
+        return accountRepository.findByIdAndUserId(accountId, userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found"));
+    }
+
+
 }
