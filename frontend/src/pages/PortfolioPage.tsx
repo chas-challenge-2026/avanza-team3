d7e1a9c6ecc4cdf777d05f66d40c08a09fcc4f79
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-08-27 11:35:33 +0200

    move account table mockdata to own file, remove content from baselayout

--- frontend/src/pages/PortfolioPage.tsx
+++ frontend/src/pages/PortfolioPage.tsx
@@ -1,10 +1,18 @@
-function PortfolioPage(){
+import { accountRows, accountColumns } from "../data/accountsData";
+import DataTable from "../components/DataTable";
 
-    return (
-        <>
-            <h1>Min Portfolio</h1>
-        </>
-    )
+function PortfolioPage() {
+  return (
+    <>
+      <h1>Min Portfolio</h1>
+      <DataTable
+        width="600px"
+        title="Konton"
+        rows={accountRows}
+        columns={accountColumns}
+      />
+    </>
+  );
 }
 
 export default PortfolioPage;
