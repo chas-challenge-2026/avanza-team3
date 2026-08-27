937270a6c41df32f986510e3765caeb4639d002b
kostas900402 <nascoskostas@gmail.com>
2026-08-27 12:26:44 +0200

    getAccountsByUserId()

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/service/AccountService.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/service/AccountService.java
@@ -1,6 +1,9 @@
 package se.comerit.avanza.account.service;
 
+import se.comerit.avanza.account.model.Account;
 import se.comerit.avanza.account.repository.AccountRepository;
+
+import java.util.List;
 
 public class AccountService {
 
@@ -8,5 +11,9 @@ public class AccountService {
 
     public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAccountsByUserId(Integer userId) {
+        return accountRepository.findByUserIdOrderByAccountTypeAscAccountNameAsc(userId);
     }
 }
