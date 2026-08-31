20b21e95bc214026c6770a2e80d8d2fb765aace2
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-08-31 10:50:07 +0200

    add gauge chart and update styling

---
+++ frontend/src/components/PortfolioHealth.tsx
@@ -0,0 +1,43 @@
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+import { NavLink } from "react-router-dom";
+import styles from "./PortfolioHealth.module.css";
+
+type PortfolioHealthProps = {
+  value: number;
+  status: "God" | "Okej" | "Dålig";
+};
+
+const PortfolioHealth = ({ value, status }: PortfolioHealthProps) => {
+  const statusColors = {
+    God: "var(--text-green)",
+    Okej: "var(--text-warning)",
+    Dålig: "var(--text-error)",
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h3>Portföljhälsa</h3>
+        <Gauge
+          width={300}
+          height={200}
+          value={value}
+          text={status}
+          startAngle={-90}
+          endAngle={90}
+          sx={{
+            [`& .${gaugeClasses.valueArc}`]: {
+              fill: statusColors[status],
+            },
+            [`& .${gaugeClasses.valueText}`]: {
+              transform: "translateY(-40px)",
+            },
+          }}
+        />
+        <NavLink to="/trygghetsoversikt"> Klicka för att läsa mer</NavLink>
+      </div>
+    </>
+  );
+};
+
+export default PortfolioHealth;
--- frontend/src/pages/PortfolioPage.tsx
+++ frontend/src/pages/PortfolioPage.tsx
@@ -1,6 +1,7 @@
 import { accountRows, accountColumns } from "../data/accountsData";
 import DataTable from "../components/DataTable";
 import AppCard from "../components/AppCard";
+import PortfolioHealth from "../components/PortfolioHealth";
 import styles from "./PorfolioPage.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
@@ -35,10 +36,7 @@ function PortfolioPage() {
 
         {/* Column 2 */}
         <div className={styles.col2}>
-          <AppCard>
-            <p className={styles.label}>Totalt värde (SEK)</p>
-            <p className={styles.value}>712 567</p>
-          </AppCard>
+          <PortfolioHealth value={60} status="God" />
 
           <DataTable
             width="600px"
