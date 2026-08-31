852d2982a1589a9bfc9e13c1c9b596b3b7a2c8b4
kostas900402 <nascoskostas@gmail.com>
2026-08-31 13:44:24 +0200

    dto blir recordklass, kommer skapa exakt efter hur det fungerar nu och lägga på validering så det blir klart

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/holding/dto/HoldingRequest.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/holding/dto/HoldingRequest.java
@@ -1,37 +1,19 @@
 package se.comerit.avanza.holding.dto;
 
-public class HoldingRequest {
-
-    /*
-    DTO = Data Transfer Object.
-
-    Den används för information som skickas mellan
-    frontend och backend.
-
-    Vi skickar INTE hela Holding-entiteten direkt.
-
-    Om frontend bara behöver skicka:
-    - instrumentId
-    - quantity
-    - accountId
-
-    så innehåller requesten bara dessa värden.
-
-    Det gör API:t tydligare och minskar risken att klienten
-    kan skicka eller ändra information som den inte ska kunna påverka.
-
-    */
-
-/*
-Ett dto kan se ut så här och validering här ser ut som ovan name
-package se.comerit.avanza.holding.dto;
+import java.math.BigDecimal;
 
 public record HoldingRequest(
-@NotNull <-validering
-String name,
-Long id
-)
 
-{}
-*/
-}
+        Integer accountId,
+
+        String ticker,
+
+        String instrumentName,
+
+        BigDecimal quantity,
+
+        BigDecimal avgBuyPrice,
+
+        String currency
+
+) {}
