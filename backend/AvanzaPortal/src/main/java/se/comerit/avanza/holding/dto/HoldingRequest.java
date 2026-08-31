01333402907cd49cf55deab221284c2d1dc6e14a
kostas900402 <nascoskostas@gmail.com>
2026-08-31 13:55:52 +0200

    validation i dto, pom uppdaterad med validation,

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/holding/dto/HoldingRequest.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/holding/dto/HoldingRequest.java
@@ -1,19 +1,36 @@
 package se.comerit.avanza.holding.dto;
 
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 
 public record HoldingRequest(
 
+        @NotNull
         Integer accountId,
 
+        @NotBlank
+        @Size(max = 30)
         String ticker,
 
+        @NotBlank
+        @Size(max = 100)
         String instrumentName,
 
+        @NotNull
+        @DecimalMin(value = "0.00000001")
         BigDecimal quantity,
 
+        @NotNull
+        @DecimalMin(value = "0")
         BigDecimal avgBuyPrice,
 
+        @NotBlank
+        @Size(max = 3)
         String currency
 
 ) {}
