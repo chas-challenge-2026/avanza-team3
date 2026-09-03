21a69a7f37104cb670d8dc59ca65d76e2316fca6
kostas900402 <nascoskostas@gmail.com>
2026-09-03 09:53:25 +0200

    restcontroller

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/controller/AccountController.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/controller/AccountController.java
@@ -1,7 +1,17 @@
 package se.comerit.avanza.account.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.comerit.avanza.account.service.AccountService;
 
-@Controller
+@RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 }
