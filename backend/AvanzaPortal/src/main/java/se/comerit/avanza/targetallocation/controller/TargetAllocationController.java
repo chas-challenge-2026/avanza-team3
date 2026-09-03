63d2e2c5baca690bc66f4226958cf39522c321cc
kostas900402 <nascoskostas@gmail.com>
2026-09-03 11:45:08 +0200

    TargetAllocationController med RestController

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/targetallocation/controller/TargetAllocationController.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/targetallocation/controller/TargetAllocationController.java
@@ -1,4 +1,17 @@
 package se.comerit.avanza.targetallocation.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.comerit.avanza.targetallocation.service.TargetAllocationService;
+
+@RestController
+@RequestMapping("/api/targetallocations")
 public class TargetAllocationController {
+
+    private final TargetAllocationService targetAllocationService;
+
+    public TargetAllocationController(TargetAllocationService targetAllocationService) {
+        this.targetAllocationService = targetAllocationService;
+    }
 }
