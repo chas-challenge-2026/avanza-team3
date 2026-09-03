10a246e3a0ac41ecd197852d215843399cae5a3a
kostas900402 <nascoskostas@gmail.com>
2026-09-03 11:26:16 +0200

    findByUserIdOrderByAccountTypeAsc() med pagination i repo

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/targetallocation/repository/TargetAllocationRepository.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/targetallocation/repository/TargetAllocationRepository.java
@@ -1,5 +1,7 @@
 package se.comerit.avanza.targetallocation.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.comerit.avanza.targetallocation.model.TargetAllocation;
@@ -11,6 +13,8 @@ import java.util.Optional;
 public interface TargetAllocationRepository extends JpaRepository<TargetAllocation, Integer> {
 
     List<TargetAllocation> findByUserIdOrderByAccountTypeAsc(Integer userId);
+
+    Page<TargetAllocation> findByUserIdOrderByAccountTypeAsc(Integer userId, Pageable pageable);
 
     Optional<TargetAllocation> findByIdAndUserId(Integer id, Integer userId);
 
