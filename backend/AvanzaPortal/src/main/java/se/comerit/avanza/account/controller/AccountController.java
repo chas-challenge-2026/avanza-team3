07941e6cb6b662e37db043afb6cdfc5413207728
kostas900402 <nascoskostas@gmail.com>
2026-09-03 10:16:55 +0200

    oanvända imports togs bort, formatering av kod lite grann bara.

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/controller/AccountController.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/account/controller/AccountController.java
@@ -3,15 +3,12 @@ package se.comerit.avanza.account.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import se.comerit.avanza.account.dto.AccountResponse;
 import se.comerit.avanza.account.service.AccountService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -24,7 +21,10 @@ public class AccountController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<AccountResponse>> getAccounts(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size, HttpSession httpSession) {
+    public ResponseEntity<Page<AccountResponse>> getAccounts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size,
+            HttpSession httpSession) {
         Integer userId = (Integer) httpSession.getAttribute("userId");
         if (userId == null) {
             return ResponseEntity.status(401).build();
