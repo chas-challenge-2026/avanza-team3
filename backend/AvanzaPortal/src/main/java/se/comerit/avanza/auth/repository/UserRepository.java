c1c4f85ca57868f57aa60cc8b761d7ec9c928094
tildaniva <tilda.niva@gmail.com>
2026-09-06 11:51:56 +0200

    Changed UserRepository

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/auth/repository/UserRepository.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/auth/repository/UserRepository.java
@@ -3,10 +3,8 @@ package se.comerit.avanza.auth.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import se.comerit.avanza.auth.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmailAndPasswordMd5(String email, String passwordMd5);
+    Optional<User> findByEmail(String email);
 }
