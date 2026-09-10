a1ad69171d07b485c327c2e0b8c8a35cc3615ba2
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-09-10 16:06:30 +0200

    rempove unused code

--- frontend/src/components/NotificationCard.tsx
+++ frontend/src/components/NotificationCard.tsx
@@ -1,39 +1,8 @@
 import styles from "./NotificationCard.module.css";
 import AppCard from "./AppCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleCheck,
-  faTriangleExclamation,
-  faCircleInfo,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 import type { Alert } from "../types/dashboard";
-
-/* const mockNotifications = [
-  {
-    id: 1,
-    type: "success",
-    icon: faCircleCheck,
-    title: "Utdelning mottagen",
-    message: "Avanza: 1 240 kr från Investor B.",
-    time: "Idag 09:14",
-  },
-  {
-    id: 2,
-    type: "warning",
-    icon: faTriangleExclamation,
-    title: "Hög exponering",
-    message: "Din risknivå är 2% från mål.",
-    time: "Igår 17:30",
-  },
-  {
-    id: 3,
-    type: "info",
-    icon: faCircleInfo,
-    title: "Årsrapport tillgänglig",
-    message: "Ericsson AB publicerade sin Q3-rapport.",
-    time: "Mån 11:00",
-  },
-]; */
 
 type NotificationCardProps = {
   alerts: Alert[];
