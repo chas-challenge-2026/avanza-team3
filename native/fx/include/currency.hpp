1026b7b4e889376d10a582713fbfd842f068eba7
jonakack <jonathan.kacker@gmail.com>
2026-09-17 12:34:17 +0200

    Added getters/setters

--- native/fx/include/currency.hpp
+++ native/fx/include/currency.hpp
@@ -8,6 +8,16 @@ class Currency
     Currency() = default;
     Currency(std::string date, std::string base, std::string name, double rate);
 
+    void SetDate(std::string &date) { this->date = date; }
+    void SetBase(std::string &base) { this->base = base; }
+    void SetName(std::string &name) { this->name = name; }
+    void SetRate(double rate) { this->rate = rate; }
+
+    std::string GetDate() { return date; }
+    std::string GetBase() { return base; }
+    std::string GetName() { return name; }
+    double GetRate() const { return rate; }
+
   private:
     std::string date;  // "2026-09-15"
     std::string base;  // "EUR"
