99e7af2e108c2067bb1078e780d8b81c4373e8d2
jonakack <jonathan.kacker@gmail.com>
2026-09-21 11:54:51 +0200

    Added comment for FxConvert

--- native/fx/include/fx.hpp
+++ native/fx/include/fx.hpp
@@ -16,4 +16,15 @@ enum
     extern "C"
 #endif
 
+/**
+ * @brief Converts exchange values. Gets the latest exchange data
+ * via HTTP.
+ *
+ * @param[in]  amount The amount that will be converted
+ * @param[in]  from The base currency to convert from
+ * @param[in]  to The currency to convert to
+ * @param[out] out A double pointer that will store the converted value
+ *
+ * @return Error code
+ */
 int FxConvert(double amount, const char *from, const char *to, double *out);
