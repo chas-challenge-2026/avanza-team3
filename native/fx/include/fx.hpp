a897c2e9ac3d851d199e6a70c4425ad4b8124a5b
jonakack <jonathan.kacker@gmail.com>
2026-09-21 10:53:15 +0200

    Added FxConvert function

--- native/fx/include/fx.hpp
+++ native/fx/include/fx.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
-#include <unordered_map>
-#include <string>
 #include "currency.hpp"
+#include "http.hpp"
 
-std::unordered_map<std::string, Currency> FxParseList(const std::string &str);
+enum
+{
+    FX_OK = 0,
+    FX_ERROR_NULL,
+    FX_ERROR_REQUEST_FAILED,
+    FX_ERROR_UNKNOWN_CURRENCY,
+    FX_ERROR_INTERNAL
+};
+
+#ifdef __cplusplus
+    extern "C"
+#endif
+
+int FxConvert(double amount, const char *from, const char *to, double *out);
--- native/fx/src/fx.cpp
+++ native/fx/src/fx.cpp
@@ -1,6 +1,9 @@
 #include "fx.hpp"
 #include "http.hpp"
+
+#include <algorithm>
 #include <iostream>
+#include <unordered_map>
 
 std::unordered_map<std::string, Currency> FxParseList(const std::string &str)
 {
@@ -84,28 +87,44 @@ std::unordered_map<std::string, Currency> FxParseList(const std::string &str)
     return list;
 }
 
-// API used: https://frankfurter.dev/
-// Get list of all currencies: https://api.frankfurter.dev/v2/rates
-// Get rate for currency pair: https://api.frankfurter.dev/v2/rate/sek/usd
+int FxConvert(double amount, const char *from, const char *to, double *out)
+{
+    if (!from || !to || !out)
+        return FX_ERROR_NULL;
+
+    try
+    {
+        std::string target = to;
+        std::transform(target.begin(), target.end(), target.begin(),
+                       [](unsigned char c) { return std::toupper(c); });
+
+        std::string url = "https://api.frankfurter.dev/v2/rates?base=";
+        url.append(from);
+
+        HttpClient client;
+        std::string buf;
+        if (client.Get(url, buf) != CURLE_OK)
+            return FX_ERROR_REQUEST_FAILED;
+
+        auto list = FxParseList(buf);
+
+        auto it = list.find(target);
+        if (it == list.end())
+            return FX_ERROR_UNKNOWN_CURRENCY;
+
+        *out = amount * it->second.GetRate();
+        return FX_OK;
+    }
+    catch (...)
+    {
+        return FX_ERROR_INTERNAL;
+    }
+}
 
 int main()
 {
-    HttpClient client;
-    std::string buf;
-    const int res = client.Get("https://api.frankfurter.dev/v2/rates", buf);
-    std::cout << "res: " << res << std::endl;
-    std::cout << "buf: " << buf << std::endl;
-
-    auto list = FxParseList(buf);
-
-    for (auto &[fst, snd] : list)
-    {
-        std::cout << "Key: " << fst << std::endl
-                  << "Value: " << snd.GetDate() << std::endl
-                  << snd.GetBase() << std::endl
-                  << snd.GetName() << std::endl
-                  << snd.GetRate() << std::endl;
-    }
-
-    return res;
+    double res;
+    int err = FxConvert(100, "usd", "sek", &res);
+    std::cout << err << std::endl;
+    std::cout << res << std::endl;
 }
