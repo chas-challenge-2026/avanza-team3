1a5ef5b8aa0c9ace54369002c9498d4cc57defa8
jonakack <jonathan.kacker@gmail.com>
2026-09-23 16:59:33 +0200

    Updated comment

--- native/fx/include/fx.hpp
+++ native/fx/include/fx.hpp
@@ -22,6 +22,7 @@ enum
  * @param[in]  to The currency to convert to
  * @param[out] out A double pointer that will store the converted value
  *
- * @return Error code
+ * @return FX_Error code.
+ * List of error codes are found in fx/README.md
  */
 int FxConvertPair(double amount, const char *from, const char *to, double *out);
