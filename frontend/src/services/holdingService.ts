cc909c50b57b1e979371f14333fdfc78dacfc7a1
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-09-23 15:34:36 +0200

    added account name and type

--- frontend/src/services/holdingService.ts
+++ frontend/src/services/holdingService.ts
@@ -1,75 +1,79 @@
-import type { Holding, HoldingRequest, HoldingApiResponse } from "../types/Holding";
+import type {
+  Holding,
+  HoldingRequest,
+  HoldingApiResponse
+} from "../types/Holding";
 
 export const getHoldings = async (): Promise<Holding[]> => {
-    const token = localStorage.getItem("token");
-  
-    if (!token) {
-      throw new Error("Ingen token hittades");
-    }
-  
-    const response = await fetch("/api/holdings", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  
-    if (!response.ok) {
-      throw new Error(`Kunde inte hämta holdings: ${response.status}`);
-    }
-  
-    const data = await response.json();
-    return data.content.map((holding: HoldingApiResponse) => ({
-      id: holding.id,
-      accountId: holding.account_id,
-      ticker: holding.ticker,
-      instrumentName: holding.instrument_name,
-      quantity: holding.quantity,
-      avgBuyPrice: holding.avg_buy_price,
-      currentPrice: holding.currentPrice,
-      currency: holding.currency,
-    }));
-  };
+  const token = localStorage.getItem("token");
 
-export const createHolding = async (
-    holding: HoldingRequest
-  ): Promise<void> => {
-    const token = localStorage.getItem("token");
-  
-    if (!token) {
-      throw new Error("Ingen token hittades");
-    }
-  
-    const response = await fetch("/api/holdings", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(holding),
-    });
-  
-    if (!response.ok) {
-      throw new Error("Could not create holding");
-    }
-  };
+  if (!token) {
+    throw new Error("Ingen token hittades");
+  }
 
-export const deleteHolding = async (
-    holdingId: number
-):Promise<void> => {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-        throw new Error('No token found')
+  const response = await fetch("/api/holdings", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`
     }
+  });
 
-    const response = await fetch(`/api/holdings/${holdingId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },    
-    });
-    if (!response.ok) {
-        throw new Error("Delete response failed");
-      }
+  if (!response.ok) {
+    throw new Error(`Kunde inte hämta holdings: ${response.status}`);
+  }
+
+  const data = await response.json();
+  console.log(data);
+
+  return data.content.map((holding: HoldingApiResponse) => ({
+    id: holding.id,
+    accountId: holding.account_id,
+    accountName: holding.account_name,
+    accountType: holding.account_type,
+    ticker: holding.ticker,
+    instrumentName: holding.instrument_name,
+    quantity: holding.quantity,
+    avgBuyPrice: holding.avg_buy_price,
+    currentPrice: holding.currentPrice,
+    currency: holding.currency
+  }));
+};
+
+export const createHolding = async (holding: HoldingRequest): Promise<void> => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    throw new Error("Ingen token hittades");
+  }
+
+  const response = await fetch("/api/holdings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(holding)
+  });
+
+  if (!response.ok) {
+    throw new Error("Could not create holding");
+  }
+};
+
+export const deleteHolding = async (holdingId: number): Promise<void> => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    throw new Error("No token found");
+  }
+
+  const response = await fetch(`/api/holdings/${holdingId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  if (!response.ok) {
+    throw new Error("Delete response failed");
+  }
 };
--- frontend/src/types/Holding.ts
+++ frontend/src/types/Holding.ts
@@ -1,30 +1,34 @@
 export type Holding = {
-    id: number;
-    accountId: number;
-    ticker: string;
-    instrumentName: string;
-    quantity: number;
-    avgBuyPrice: number;
-    currentPrice: number;
-    currency: string;
-  };
-  
-  export interface HoldingRequest {
-    accountId: number;
-    ticker: string;
-    instrumentName: string;
-    quantity: number;
-    avgBuyPrice: number;
-    currency: string;
-  }
+  id: number;
+  accountId: number;
+  accountName: string;
+  ticker: string;
+  instrumentName: string;
+  accountType: string;
+  quantity: number;
+  avgBuyPrice: number;
+  currentPrice: number;
+  currency: string;
+};
 
- export type HoldingApiResponse = {
-    id: number;
-    account_id: number;
-    ticker: string;
-    instrument_name: string;
-    quantity: number;
-    avg_buy_price: number;
-    currentPrice: number;
-    currency: string;
-  };
+export interface HoldingRequest {
+  accountId: number;
+  ticker: string;
+  instrumentName: string;
+  quantity: number;
+  avgBuyPrice: number;
+  currency: string;
+}
+
+export type HoldingApiResponse = {
+  id: number;
+  account_id: number;
+  account_name: string;
+  account_type: string;
+  ticker: string;
+  instrument_name: string;
+  quantity: number;
+  avg_buy_price: number;
+  currentPrice: number;
+  currency: string;
+};
